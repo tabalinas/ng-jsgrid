ba43222056e21ddce29666bc2dcff31906862528
Artem Tabalin <tabalinas@gmail.com>
2016-04-15 22:08:13 +0200

    [Core] Basic directive implementation

---
+++ src/ng-jsgrid.core.js
@@ -0,0 +1,23 @@
+(function() {
+   "use strict";
+
+    angular.module("angular.jsgrid", [])
+        .directive("ngJsgrid", function() {
+            return {
+                restrict: "A",
+                scope: {
+                    config: "=ngJsgrid"
+                },
+                link: function(scope, $element, attrs) {
+
+                    $.each(scope.config, function(key) {
+                        scope.$watch("config." + key, function(value) {
+                            $element.jsGrid("option", key, value);
+                        });
+                    });
+
+                    $element.jsGrid(scope.config);
+                }
+            };
+        });
+})();
