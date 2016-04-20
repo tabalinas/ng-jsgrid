fed407577b7d67617dd15ed655e52761ea8cf8e9
Artem Tabalin <tabalinas@gmail.com>
2016-04-20 09:37:33 +0200

    Tests: Adds for basic implementation

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,46 @@
 
 describe('ng-grid', function() {
     var $compile;
-    var $rootScope;
+    var $scope;
 
     beforeEach(module('angular.jsgrid'));
 
     beforeEach(inject(function(_$compile_, _$rootScope_){
         $compile = _$compile_;
-        $rootScope = _$rootScope_;
+        $scope = _$rootScope_;
     }));
 
-    it('Replaces the element with the appropriate content', function() {
-        expect(true).toBe(true);
+    it('should create jsGrid providing options', function() {
+        // arrange
+        var data = [];
+        $scope.config = {
+            data: data
+        };
+
+        // act
+        var element = $compile('<div ng-jsgrid="config"></div>')($scope);
+        $scope.$digest();
+
+        // assert
+        expect($(element).data('JSGrid')).toBeDefined();
+        expect($(element).jsGrid('option', 'data')).toBe(data);
     });
+
+    it('should change option when model property is changed', function() {
+        // arrange
+        $scope.config = {
+            data: []
+        };
+        var element = $compile('<div ng-jsgrid="config"></div>')($scope);
+        $scope.$digest();
+        var newModelValue = [];
+
+        // act
+        $scope.config.data = newModelValue;
+        $scope.$digest();
+
+        // assert
+        expect($(element).jsGrid('option', 'data')).toBe(newModelValue);
+    });
+
 });
